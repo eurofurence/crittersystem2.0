94156590e308f5017dac38927157ed653f9456a0
lionwolfdev <80899291+lionwolfdev@users.noreply.github.com>
2026-08-15 15:52:41 +0200

    Add invite lookup by user

--- src/Repository/InviteTokenRepository.php
+++ src/Repository/InviteTokenRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\InviteToken;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -19,5 +20,10 @@ class InviteTokenRepository extends ServiceEntityRepository
     public function findOneByToken(string $token): ?InviteToken
     {
         return $this->findOneBy(['token' => $token]);
+    }
+
+    public function findOneByUser(User $user): ?InviteToken
+    {
+        return $this->findOneBy(['user' => $user]);
     }
 }
